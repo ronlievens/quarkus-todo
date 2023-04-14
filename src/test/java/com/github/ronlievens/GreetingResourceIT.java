cb2815cdf3d8b6aa155b54a227ead30c981da96f
Ron Lievens <ronlievens@outlook.com>
2023-04-14 07:34:11 +0200

    init project

---
+++ src/test/java/com/github/ronlievens/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package com.github.ronlievens;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
